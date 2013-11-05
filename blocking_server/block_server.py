15217e607413a7f0a3a2008b2431e16d786d5c54
acmerfight <acmerfight@gmail.com>
2013-11-05 16:34:41 +0800

    block_server

---
+++ blocking_server/block_server.py
@@ -0,0 +1,30 @@
+import socket
+
+
+text_content = """
+HTTP/1.x 200 OK
+Content-Type: text/html
+
+<head>
+    <title>WOW</title>
+</head>
+<html>
+    <p>Hello World!</p>
+</html>
+"""
+
+HOST = "127.0.0.1"
+PORT = 8080
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((HOST, PORT))
+server_socket.listen(5)
+
+while 1:
+    client_socket, addresss = server_socket.accept()
+    request = client_socket.recv(1024)
+    try:
+        client_socket.sendall(text_content)
+    except socket.error:
+        client_socket.close()
+    client_socket.close()
