ba6e8797416aba902df4cec979e73bee952a7a84
acmerfight <acmerfight@gmail.com>
2013-11-07 14:30:00 +0800

    add process server

---
+++ blocking_server/process_server.py
@@ -0,0 +1,43 @@
+import socket
+import time
+from multiprocessing import Process
+
+import requests
+
+text_content = """
+HTTP/1.x 200 OK
+Content-Type: text/html
+
+<head>
+    <title>WOW</title>
+</head>
+<html>
+    <p>Hello World!</p>
+</html>
+"""
+
+def server():
+    HOST = "127.0.0.1"
+    PORT = 9000
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((HOST, PORT))
+    server_socket.listen(5)
+
+    while 1:
+        client_socket, _ = server_socket.accept()
+        t = Process(target=handle_request, args=(client_socket, text_content))
+        t.start()
+
+def handle_request(client_socket, text_content):
+    client_socket.recv(1024)
+    try:
+        client_socket.sendall(text_content)
+    except socket.error:
+        client_socket.close()
+        return
+    client_socket.close()
+
+
+if __name__ == "__main__":
+    server()
